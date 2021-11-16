b131548136527de3d88278533590745bc7856e53
tasengwong <akhom.tasaengwong@gmail.com>
2021-11-16 10:25:50 +0700

    first commit

---
+++ introduction.js
@@ -0,0 +1,1 @@
+console.log('start, Learn Basic JavaSript')
---
+++ main.js
@@ -0,0 +1,12 @@
+let firstname = 'Akhom'
+let lastname = 'tasaengwong'
+let country = 'thailand'
+let city = 'chaingmai'
+let age = '22'
+let isMarried = true
+console.log (firstname, lastname, country, city, age, isMarried)
+
+const gravitty = 9.81
+const boilingPoint = 100
+const PI = 3.14
+console.log(gravitty,boilingPoint,PI)
