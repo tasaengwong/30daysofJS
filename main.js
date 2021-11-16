35a78dc98a8ecedf74da5108391b49118fe78dfb
tasengwong <akhom.tasaengwong@gmail.com>
2021-11-16 11:37:55 +0700

    day 1 introduction

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ let lastname = 'tasaengwong'
 let marital = 'single'
 let country = 'thailand' 
 let city = 'chaingmai' 
-let age = '22' 
-console.log (firstname, lastname, marital, country, city, age)  
+let age = 22 
+console.log (firstname, lastname, marital, country, city, age)  //Declare variables single lines
 
-let Name = 'akhom', LastName='tasaengwong',Maritalstatus = 'single',Country='Thailand',Age = 22
+let Name = 'akhom', LastName='tasaengwong',Maritalstatus = 'single',Country='Thailand',Age = 22   //Declare variablles multiple lines
 console.log(Name, LastName, Maritalstatus, Country, Age)
+
+
+const myAge = 22
+const yourAge = 25
+console.log(myAge, yourAge)
 
