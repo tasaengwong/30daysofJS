7faa761eea527b8a4c068a37b3b641e37e70eb2d
tasengwong <akhom.tasaengwong@gmail.com>
2021-11-18 10:10:09 +0700

    day 2 substr

--- substr.js
+++ substr.js
@@ -1,2 +1,6 @@
 let stringsunb= "You cannot end a sentence with because because because is a conjunction"
-console.log(stringsub.substr())
+console.log(stringsunb.substr(0,38))
+
+let count = 'Love is the best thing in this world. Some found their love and some are still looking for their love.' 
+console.log(count)
+// console.log(count.incl("love"));
