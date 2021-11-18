8629e929efaa2c0d9f09332c69f6c4a188a803b4
tasaengwong <Akhom@Siwats-iMac.20ScoopsCNX>
2021-11-18 13:20:09 +0700

    day 2 clean string

--- substr.js
+++ substr.js
@@ -11,3 +11,15 @@ New_sentence = sentence.replace(/$|%|&|#|\$|\@|/gi,'');
 // console.log(sentence.replace(/$|%|\@/gi,''))
 // New_sentence = 'hello test'
 console.log(New_sentence)
+
+
+let salary = 5000;
+let bonus = 10000;
+let online = 15000;
+
+const summary = salary * 12;
+const sum_bonus = summary + 10000;
+const online_course = online * 12;
+const total = sum_bonus + online_course;
+
+console.log(total)
