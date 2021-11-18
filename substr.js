e549f484a310482e0b00c83559003199a2a4fe5d
tasaengwong <Akhom@Siwats-iMac.20ScoopsCNX>
2021-11-18 11:47:00 +0700

    day 2 clean string

--- substr.js
+++ substr.js
@@ -4,3 +4,9 @@ console.log(stringsunb.substr(0,38))
 let count = 'Love is the best thing in this world. Some found their love and some are still looking for their love.' 
 console.log(count)
 // console.log(count.incl("love"));
+
+
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+// New_sentence = sentence.replace(/$|%|\@/gi,''); 
+New_sentence = 'hello test'
+console.log(New_sentence)
