6e99566edc95cfd714ac99780016df6242be1300
tasaengwong <Akhom@Admins-iMac.local>
2021-11-23 15:49:04 +0700

    day5 create folder

--- day5/day5.js
+++ day5/day5.js
@@ -69,3 +69,4 @@ console.log(itCompanies.splice())
 
 
 //level2 
+
---
+++ day5/master.js
@@ -0,0 +1,7 @@
+
+console.log(countries)
+console.log(webTechs)
+
+let text ='I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+console.log(words)
+console.log(words.length)
