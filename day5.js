7ad78c390720bd25b5ef5c0c03cdc85b8353df18
tasaengwong <Akhom@Admins-iMac.local>
2021-11-23 13:35:10 +0700

    day5 Array

---
+++ day5.js
@@ -0,0 +1,24 @@
+const countries = ["Albania","Bolivia", "Canada","Denmark","Ethiopia","Finland","Germany","Hungary","Ireland","Japan","Kenya", ""
+];
+
+const webTechs = ["HTML","CSS","JavaScript","React","Redux","Node", "MongoDB", ""
+];
+
+const techs = ["Num","Text","Boolean","Array","Condition","True","false"]
+
+console.log(techs.length);
+console.log(techs[0])
+console.log(techs.lastIndexOf('Num'))
+
+const mixedDataTypes =[25,'test',false,null,undefined]
+console.log('Number of numbers:',mixedDataTypes.length)
+const itCompanies = ['facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+console.log(itCompanies)
+
+console.log('Number of Companies is :',itCompanies.length)
+console.log(itCompanies[0])
+console.log(itCompanies[3])
+console.log(itCompanies[6])
+
+
+
