0bcb5c873d7cfe1db2944df8c3d542cda75b6091
tasaengwong <Akhom@Admins-iMac.local>
2021-11-24 08:29:52 +0700

    day5 update file

--- day5/master.js
+++ day5/master.js
@@ -1,6 +1,6 @@
 
 console.log(countries)
-console.log(webTechs)
+console.log(webTechs);
 
 let text ='I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
 let words = text.split(" ");
@@ -60,3 +60,5 @@ console.log("average: ", sum / len);
 
 
 console.log(countries.length)
+
+  
